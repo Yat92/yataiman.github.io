e6a09346aa4897f3b7df832558699a6843b3cbde
Yat92 <warframervalk@hotmail.com>
2024-09-01 21:27:06 +0800

    Addindex.html

---
+++ index.h
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Temporary Domain</title>
+</head>
+<body>
+    <h1>Welcome to My Temporary Domain</h1>
+    <p>This is a placeholder for setting up Google Workspace.</p>
+</body>
+</html>
